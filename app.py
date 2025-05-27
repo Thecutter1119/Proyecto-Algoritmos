a752425e64f7072e9ad395c9eb6cd01028f01715
jhon esneider marin bolivar <jhonsneidermarin@gmail.com>
2025-05-27 16:51:30 -0500

    Cambios minimos

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from complexity_predictor import ComplexityPredictor
 app = Flask(__name__)
 predictor = ComplexityPredictor(vector_size=100)
 
-# Opcional: cargar pesos si ya entrenaste y los guardaste
-# predictor.o_model.load_weights("modelos_guardados/o_model.h5")
-# predictor.omega_model.load_weights("modelos_guardados/omega_model.h5")
-# predictor.theta_model.load_weights("modelos_guardados/theta_model.h5")
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
